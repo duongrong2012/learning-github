43ba43981c93eda2ef7376a7656b5fcec700d351
duongrong2012 <duongrong2012@gmail.com>
2025-04-02 10:54:25 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// Định nghĩa một route cơ bản
+app.get('/', (req, res) => {
+    res.send('Hello World từ Express!');
+});
+
+// Lắng nghe trên cổng 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server đang chạy trên cổng txtd, ${PORT}`);
+});
