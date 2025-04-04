93d26dde633833bab383f6f23199b6f1abe44b73
duongrong2012 <duongrong2012@gmail.com>
2025-04-04 19:13:38 +0700

    add g to feat/task6

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('Hello World từ Expresse!d');
 });
-console.log('main 2')
+console.log('main 3')
 
 console.log("Hello World từ Express!");
 
